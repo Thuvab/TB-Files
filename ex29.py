44e4403602491bb151a6a4618bb16cb25152271a
Thuva <thuv_a@yahoo.com.au>
2017-05-11 22:47:35 +1000

    exercises

---
+++ ex29.py
@@ -0,0 +1,28 @@
+people = 20
+cats = 30
+dogs = 15
+
+
+if people < cats:
+    print "too many cats.. the world is ruinee"
+
+if people > cats:
+    print "not many cats the world is daved"
+
+if people < dogs:
+    print "The world is drooled on"
+
+if people > dogs:
+    print "The world is dey"
+
+dogs += 5
+
+if people >= dogs:
+    print "people are greater than or equel to dogs"
+
+if people <= dogs:
+    print "People are less than or equal to dogs."
+
+if people == dogs:
+    print "people are dogs"
+    
